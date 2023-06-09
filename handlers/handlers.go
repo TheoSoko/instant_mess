4f95f42f59d6cfe744746eb805db63c069c9dc8b
TheoSoko <theo.sokolowsky@gmail.com>
2023-06-09 05:19:49 +0200

    Corrections gestion d'erreur

---
+++ handlers/handlers.go
@@ -0,0 +1,103 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/TheoSoko/instant_mess/data"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var wsUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+var activeSockets = make(map[int]*websocket.Conn)
+
+func Socketing(w http.ResponseWriter, r *http.Request) {
+	socket, err := wsUpgrader.Upgrade(w, r, nil)
+	defer socket.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	intId, err := strconv.Atoi(id)
+	if id == "" || err != nil {
+		socket.WriteMessage(5, []byte("You need to provide and integer value ID as a query parameter"))
+		socket.Close()
+		return
+	}
+
+	activeSockets[intId] = socket
+	defer delete(activeSockets, intId)
+
+	for {
+		_, p, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Println("erreur sur la lecture d'un message, ou fermeture du ws:", err)
+			return
+		}
+		//fmt.Println("Message du client:", p)
+		returnMessage := []byte(fmt.Sprint("We received your message ! It's : \"", string(p), "\""))
+		socket.WriteMessage(1, returnMessage)
+	}
+}
+
+func SendMessage(w http.ResponseWriter, r *http.Request) {
+	id, ok := mux.Vars(r)["id"]
+	friendId, ok2 := mux.Vars(r)["friendId"]
+	if !ok || !ok2 {
+		w.WriteHeader(400)
+		w.Write([]byte("Please, provide the user id and the friend's id in the url"))
+		return
+	}
+
+	intId, err := strconv.Atoi(id)
+	intFriendId, err2 := strconv.Atoi(friendId)
+	if err != nil || err2 != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("Please, provide the user id and the friend's id as integer values"))
+		return
+	}
+
+	user := data.GetUser(intId)
+	if !user.Friends[intFriendId] {
+		w.WriteHeader(400)
+		w.Write([]byte("Sorry, you can't send a message to a user you're not friends with"))
+		return
+	}
+
+	var payload struct {
+		Message string `json:"message"`
+	}
+	err = json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("A problem occured with the payload"))
+	}
+
+	// ** We would send the message to db here
+
+	// ** Then, we write from the socket if friend is connected
+
+	w.WriteHeader(204)
+
+	friendSocket, ok := activeSockets[intFriendId]
+	if !ok {
+		// ** Send push notification if not connected
+		return
+	}
+
+	message := fmt.Sprint("Hey, your friend ",
+		data.GetUser(intId).Firstname,
+		" just successfully sent a message through a websocket. The following : \n",
+		payload.Message,
+	)
+
+	friendSocket.WriteMessage(1, []byte(message))
+}
--- main.go
+++ main.go
@@ -3,25 +3,20 @@ package main
 import (
 	"net/http"
 
+	"github.com/TheoSoko/instant_mess/handlers"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-var activeSockets = make(map[int]*websocket.Conn)
 
 func main() {
 
 	r := mux.NewRouter()
 
 	http.Handle("/", r)
-	r.HandleFunc("/ws", socketing)
-	r.HandleFunc("/users/{id}/friends/{friendId}/message", sendMessage).Methods("POST")
+	r.HandleFunc("/ws", handlers.Socketing)
+	r.HandleFunc("/users/{id}/friends/{friendId}/message", handlers.SendMessage).Methods("POST")
 
-	err := http.ListenAndServe("127.0.0.1:6969", r)
+	err := http.ListenAndServe("127.0.0.1:4000", r)
 	if err != nil {
 		panic(err)
 	}
